21588e9a8952fae55a3f783677692b801c44e011
mmAbdullahrana <ar292211@gmail.com>
2024-08-01 12:26:56 +0500

    Added local storage

--- src/js/model.js
+++ src/js/model.js
@@ -9,23 +9,23 @@ export const storingData = function (data) {
 
 export const deleteData = function (id) {
   state.message.splice(
-    state.message.findIndex((el) => el.id === id),
+    state.message.findIndex(el => el.id === id),
     1
   );
   storeTodo();
 };
 
 const storeTodo = function () {
-  localStorage.setItem("todos", JSON.stringify(state.message));
+  localStorage.setItem('todos', JSON.stringify(state.message));
 };
 
 const init = function () {
-  const storage = localStorage.getItem("todos");
+  const storage = localStorage.getItem('todos');
   if (storage) state.message = JSON.parse(storage);
 };
 
 const clearTodos = function () {
-  localStorage.clear("todos");
+  localStorage.clear('todos');
 };
-clearTodos();
-// init();
+// clearTodos();
+init();
